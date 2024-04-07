efd3ae09b5ed5127c6a1a088072f6fae9fed84de
eduardofulgenciocomendeiro <eduardofulgenciocomendeiro@gmail.com>
2024-04-07 09:25:40 +0200

    Init content
    Added init steps for App

---
+++ AmsEfc/AmsEfc/AmsEfcApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmsEfcApp.swift
+//  AmsEfc
+//
+//  Created by efulgencio on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AmsEfcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
